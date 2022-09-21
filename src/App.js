1949bf09b8e0cf17bf44f6d0231778d33d651d85
Caitlin Croteau <cjcroteau@gmail.com>
2022-09-20 22:01:00 -0700

    add navbar component/scss and add component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Button from './components/Button'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>The page is empty for now!</h1>
       <Button onClick={() => alert("AAAHHHHHH WHY WOULD YOU CLICK THAT BUTTON?!")}>Except for this button</Button>
     </div>
