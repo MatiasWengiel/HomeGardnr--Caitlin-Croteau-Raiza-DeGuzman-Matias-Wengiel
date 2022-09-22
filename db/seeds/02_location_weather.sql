26ea83f8591ca2739698f180b7cd3c4dd46107f2
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-22 13:05:31 -0700

    Add seed for location_weather table

---
+++ db/seeds/02_location_weather.sql
@@ -0,0 +1,3 @@
+-- Seeds for Location Weather Table --
+
+INSERT INTO location_weather (location_name, local_warnings, local_rain, daily_high, daily_low) VALUES ('Vancouver', 'None', '10mm', 23, 13);
