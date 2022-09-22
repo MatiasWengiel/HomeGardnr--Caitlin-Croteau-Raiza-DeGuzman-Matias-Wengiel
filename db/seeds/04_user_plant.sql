2396fe5c8264f8e6deeffc0d08db8c8340db23fb
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-22 14:56:48 -0700

    Wrap dates in single quotes to conform with DATE data type for the columns

--- db/seeds/04_user_plant.sql
+++ db/seeds/04_user_plant.sql
@@ -1,7 +1,7 @@
 -- Seeds for User Plant Table --
 
-INSERT INTO user_plant (planted_date, last_watered_at, when_to_water_next, high_temperature_notification, low_temperature_notification, plant_id, user_id) VALUES (2022-05-11, 2022-09-22, 2022-09-23, 40, 1, 1, 1);
+INSERT INTO user_plant (planted_date, last_watered_at, when_to_water_next, high_temperature_notification, low_temperature_notification, plant_id, user_id) VALUES ('2022-05-11', '2022-09-22', '2022-09-23', 40, 1, 1, 1);
 
-INSERT INTO user_plant (planted_date, last_watered_at, when_to_water_next, high_temperature_notification, low_temperature_notification, plant_id, user_id) VALUES (2022-06-01, 2022-09-19, 2022-09-23, 35, 0, 2, 1);
+INSERT INTO user_plant (planted_date, last_watered_at, when_to_water_next, high_temperature_notification, low_temperature_notification, plant_id, user_id) VALUES ('2022-06-01', '2022-09-19', '2022-09-23', 35, 0, 2, 1);
 
-INSERT INTO user_plant(planted_date, last_watered_at, when_to_water_next, high_temperature_notification, low_temperature_notification, plant_id, user_id) VALUES (2022-05-19, 2022-09-19, 2022-09-23, 25, 10, 3, 1);
+INSERT INTO user_plant(planted_date, last_watered_at, when_to_water_next, high_temperature_notification, low_temperature_notification, plant_id, user_id) VALUES ('2022-05-19', '2022-09-19', '2022-09-23', 25, 10, 3, 1);
