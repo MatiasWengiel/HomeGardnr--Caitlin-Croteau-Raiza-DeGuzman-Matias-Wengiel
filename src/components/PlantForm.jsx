370bf359cfb548d4e5d6b1a75a49b0ac56e478a4
Caitlin Croteau <cjcroteau@gmail.com>
2022-09-23 13:07:47 -0700

    change layout to rows/columns

--- src/components/PlantForm.jsx
+++ src/components/PlantForm.jsx
@@ -2,78 +2,99 @@ import React from "react";
 import "./PlantForm.scss";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function PlantForm(props) {
   return (
-    <Form className="ms-auto">
-      <Form.Group className="mb-3" controlId="formGenericName">
-        <Form.Label>Generic Name</Form.Label>
-        <Form.Control type="text" placeholder="Generic Name" />
-        {/* <Form.Text className="text-muted">
+    <Container className="ms-auto">
+      <Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGenericName">
+            <Form.Label>Generic Name</Form.Label>
+            <Form.Control type="text" placeholder="Generic Name" />
+            {/* <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text> */}
-      </Form.Group>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formSpecificName">
-        <Form.Label>Specific Name</Form.Label>
-        <Form.Control type="text" placeholder="Specific Name" />
-      </Form.Group>
+          <Form.Group as={Col} controlId="formSpecificName">
+            <Form.Label>Specific Name</Form.Label>
+            <Form.Control type="text" placeholder="Specific Name" />
+          </Form.Group>
+        </Row>
 
-      <Form.Group className="mb-3" controlId="formPlantingSeason">
-        <Form.Label>Planting Season</Form.Label>
-        <Form.Control type="text" placeholder="Planting Season" />
-      </Form.Group>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formPlantingSeason">
+            <Form.Label>Planting Season</Form.Label>
+            <Form.Control type="text" placeholder="Planting Season" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formSeedSpacing">
-        <Form.Label>Seed Spacing</Form.Label>
-        <Form.Control type="text" placeholder="Seed Spacing" />
-      </Form.Group>
+          <Form.Group as={Col} controlId="formMaturity">
+            <Form.Label>Time to Maturity</Form.Label>
+            <Form.Control type="text" placeholder="Time to Maturity" />
+          </Form.Group>
+        </Row>
 
-      <Form.Group className="mb-3" controlId="formPlantDepth">
-        <Form.Label>Seed Planting Depth</Form.Label>
-        <Form.Control type="text" placeholder="Planting Depth" />
-      </Form.Group>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formSeedSpacing">
+            <Form.Label>Seed Spacing</Form.Label>
+            <Form.Control type="text" placeholder="Seed Spacing" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formMaturity">
-        <Form.Label>Time to Maturity</Form.Label>
-        <Form.Control type="text" placeholder="Time to Maturity" />
-      </Form.Group>
+          <Form.Group as={Col} controlId="formPlantDepth">
+            <Form.Label>Seed Planting Depth</Form.Label>
+            <Form.Control type="text" placeholder="Planting Depth" />
+          </Form.Group>
+        </Row>
 
-      <Form.Group className="mb-3" controlId="formSunlightNeeds">
-        <Form.Label>Sunlight Needs</Form.Label>
-        <Form.Control type="text" placeholder="Sunlight Needs" />
-      </Form.Group>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formHighTemp">
+            <Form.Label>Max High Temperature</Form.Label>
+            <Form.Control type="text" placeholder="Please enter a number" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formHighTemp">
-        <Form.Label>Max High Temperature</Form.Label>
-        <Form.Control type="text" placeholder="Please enter a number" />
-      </Form.Group>
+          <Form.Group as={Col} controlId="formLowTemp">
+            <Form.Label>Max Low Temperature</Form.Label>
+            <Form.Control type="text" placeholder="Please enter a number" />
+          </Form.Group>
+        </Row>
 
-      <Form.Group className="mb-3" controlId="formLowTemp">
-        <Form.Label>Max Low Temperature</Form.Label>
-        <Form.Control type="text" placeholder="Please enter a number" />
-      </Form.Group>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formSunlightNeeds">
+            <Form.Label>Sunlight Needs</Form.Label>
+            <Form.Control type="text" placeholder="Sunlight Needs" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formWaterNeeds">
-        <Form.Label>Water needs</Form.Label>
-        <Form.Select aria-label="WaterNeeds">
-          <option>Select how many days per week you want to water.</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="3">4</option>
-          <option value="3">5</option>
-          <option value="3">6</option>
-          <option value="3">Every day</option>
-        </Form.Select>
-      </Form.Group>
+          <Form.Group as={Col} controlId="formWaterNeeds">
+            <Form.Label>Water needs</Form.Label>
+            <Form.Select aria-label="WaterNeeds">
+              <option>Select how many days per week you want to water.</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="3">4</option>
+              <option value="3">5</option>
+              <option value="3">6</option>
+              <option value="3">Every day</option>
+            </Form.Select>
+          </Form.Group>
+        </Row>
 
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formPlantDescription">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Tell us about your plant!"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
