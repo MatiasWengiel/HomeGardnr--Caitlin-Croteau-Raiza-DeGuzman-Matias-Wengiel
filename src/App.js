1d7a984e9b872f87cc066317f27e67686f49ebf3
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-23 14:07:01 -0500

    Work in progress

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 import Button from './components/Button'
 import Banner from './components/Banner'
 import PlantCard from './components/PlantCard'
+import WeatherCard from './components/WeatherCard';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 
 function App() {
+  const [locationKey, setLocationKey] = useState("")
+  const location = "winnipeg"
+  useEffect(() => {
+    axios.get(`/api/location/winnipeg`).then((response) => {
+      setLocationKey(response.data)
+    })
+
+    console.log(locationKey)
+  })
+
   return (
     <div className="App">
       <h1>The page is empty for now!</h1>
       <Button onClick={() => alert("AAAHHHHHH WHY WOULD YOU CLICK THAT BUTTON?!")}>Except for this button</Button>
-      <Banner weatherWarning="Risk of frost overnight" />
-      <PlantCard plant="Tomatoes" picture="https://live.staticflickr.com/1339/1085617988_edc33bce22_z.jpg" altText="ripe tomatoes" waterStatus="watered" lastWatered="09/21/2022" nextWatering="09/23/2022" />
+      <WeatherCard currentLocation="Winnipeg, MB" localWarnings="wind warning" localHigh="10 C" localLow="5 C" localPrecipitation="10mm" />
     </div>
   );
 }
