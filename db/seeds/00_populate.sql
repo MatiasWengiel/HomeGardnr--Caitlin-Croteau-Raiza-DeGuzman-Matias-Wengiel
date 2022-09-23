6c622c0ca31d8b6a1770ee6c44703bca6fef1854
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-22 18:10:22 -0700

    Delete location_weather seed file. Update numbering of files. Update of files in 00_populate.sql

--- db/seeds/00_populate.sql
+++ db/seeds/00_populate.sql
@@ -1,4 +1,3 @@
 \i ./db/seeds/01_plants.sql
-\i ./db/seeds/02_location_weather.sql
-\i ./db/seeds/03_users.sql
-\i ./db/seeds/04_user_plant.sql
+\i ./db/seeds/02_users.sql
+\i ./db/seeds/03_user_plant.sql
