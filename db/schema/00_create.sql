a3f351c266016993aca1198cfd0e2de5450dd9c6
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-22 18:09:25 -0700

    Delete location_weather table. Update numbering in file names. Update 00_create.sql file names as well

--- db/schema/00_create.sql
+++ db/schema/00_create.sql
@@ -1,4 +1,3 @@
 \i ./db/schema/01_plants.sql
-\i ./db/schema/02_location_weather.sql
-\i ./db/schema/03_users.sql
-\i ./db/schema/04_user_plant.sql
+\i ./db/schema/02_users.sql
+\i ./db/schema/03_user_plant.sql
