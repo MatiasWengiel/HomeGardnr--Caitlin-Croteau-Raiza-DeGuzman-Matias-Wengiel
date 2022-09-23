37e1bde97b9b9e2da92a2f5154e76f86d9e9a0b0
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-23 12:24:29 -0700

    Remove file reset.sql - no longer needed

--- db/reset.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS final;
-CREATE DATABASE final;
-
-\i ./db/schema/00_create.sql
-\i ./db/seeds/00_populate.sql
--- db/schema/00_create.sql
+++
@@ -1,3 +0,0 @@
-\i ./db/schema/01_plants.sql
-\i ./db/schema/02_users.sql
-\i ./db/schema/03_user_plant.sql
