15de8aa74a7f89ef52f444360150a859760ee0f8
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-23 14:05:45 -0500

    Work in progress - attempting to get an axios call to the API working properly

--- server/routes/location_weather.js
+++ server/routes/location_weather.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const axios = require('axios')
 
-module.exports = (db) => {
-  router.get('/', (req, res) => {
-    res.send("This would be the location_weather page");
+module.exports = () => {
+  router.get('/:location', (req, res) => {
+    const location = req.params.location
+    let cityKey = ""
+    let response = ""
+    axios.get(`http://dataservice.accuweather.com/locations/v1/search?q=winnipeg&apikey=jURdoMXOLNcy90VzcO0dYyankIoJURwM`)
+      .then(reply => {
+        cityKey = reply.data[0].Key
+        console.log(cityKey)
+      })
+      .then(
+        axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${cityKey}&apikey=jURdoMXOLNcy90VzcO0dYyankIoJURwM`)
+      )
+      .then(reply => console.log(reply.data))
+
   });
   return router;
 };
