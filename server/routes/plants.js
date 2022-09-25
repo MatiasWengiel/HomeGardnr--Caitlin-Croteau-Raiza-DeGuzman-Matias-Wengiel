cbf537b7ac9ca88117607550d1780f0f614895d7
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-24 22:27:26 -0700

    Add route for plants and plants/:id

--- server/routes/plants.js
+++ server/routes/plants.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const { getPlantInfo } = require('../../db/dbqueries.js')
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
+  router.get('/plants', (req, res) => {
     res.send("This would be the plants page");
   });
+
+  router.get('/plants/:id', (req, res) => {
+    db.query(getPlantInfo(1)).then(data => {
+      console.log(data);
+      res.render(data);
+      // res.json(data.rows[0]);
+    })
+  });
+
   return router;
 };
