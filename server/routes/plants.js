7436727e12c6de576795ef50a2d5e2bdcfcd12ee
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-24 22:57:12 -0700

    Fix route for plants

--- server/routes/plants.js
+++ server/routes/plants.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const { getPlantInfo } = require('../../db/dbqueries.js')
 
 module.exports = (db) => {
-  router.get('/plants', (req, res) => {
-    res.send("This would be the plants page");
-  });
+  // router.get('/', (req, res) => {
+  //   res.send("This would be the plants page");
+  // });
 
-  router.get('/plants/:id', (req, res) => {
+  router.get('/', (req, res) => {
     db.query(getPlantInfo(1)).then(data => {
       console.log(data);
-      res.render(data);
-      // res.json(data.rows[0]);
+      res.json(data.rows);
     })
   });
 
