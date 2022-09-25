3e5e28cb3dff0248fde2123709a56f9c8ee4f92a
Caitlin Croteau <cjcroteau@gmail.com>
2022-09-25 00:16:51 -0700

    add plantInfo state, handleChange, handleSubmit functions

--- src/components/PlantForm.jsx
+++ src/components/PlantForm.jsx
@@ -8,6 +8,23 @@ import Col from "react-bootstrap/Col";
 
 export default function PlantForm(props) {
   const [validated, setValidated] = useState(false);
+  const [plantInfo, setPlantInfo] = useState({
+    genericName: "",
+    specificName: "",
+    season: "",
+    maturity: "",
+    spacing: "",
+    depth: "",
+    maxTemp: "",
+    minTemp: "",
+    sunlight: "",
+    water: "",
+    description: "",
+  });
+
+  const handleChange = (event) => {
+    setPlantInfo({ ...plantInfo, [event.target.name]: event.target.value });
+  };
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -16,9 +33,27 @@ export default function PlantForm(props) {
       event.stopPropagation();
     }
     setValidated(true);
+    event.preventDefault();
+    console.log(plantInfo);
+
+    //resets form
+    setPlantInfo({
+      genericName: "",
+      specificName: "",
+      season: "",
+      maturity: "",
+      spacing: "",
+      depth: "",
+      maxTemp: "",
+      minTemp: "",
+      sunlight: "",
+      water: "",
+      description: "",
+    });
   };
 
   //noValidate cancels browser's default validation
+  //preventDefault prevents the submit button from refreshing the page
 
   return (
     <Container
@@ -29,7 +64,13 @@ export default function PlantForm(props) {
         <Row className="mb-4 mt- ">
           <Form.Group as={Col} controlId="formGenericName">
             <Form.Label>Generic Name</Form.Label>
-            <Form.Control required type="text" placeholder="Generic Name" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Generic Name"
+              defaultValue={plantInfo.genericName}
+              onChange={handleChange}
+            />
             <Form.Control.Feedback type="invalid">
               Please enter a name.
             </Form.Control.Feedback>
@@ -37,7 +78,13 @@ export default function PlantForm(props) {
 
           <Form.Group as={Col} controlId="formSpecificName">
             <Form.Label>Specific Name</Form.Label>
-            <Form.Control required type="text" placeholder="Specific Name" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Specific Name"
+              defaultValue={plantInfo.specificName}
+              onChange={handleChange}
+            />
           </Form.Group>
           <Form.Control.Feedback type="invalid">
             Please enter a name.
@@ -47,7 +94,13 @@ export default function PlantForm(props) {
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formPlantingSeason">
             <Form.Label>Planting Season</Form.Label>
-            <Form.Control required type="text" placeholder="Ex: Spring" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Ex: Spring"
+              defaultValue={plantInfo.season}
+              onChange={handleChange}
+            />
             <Form.Control.Feedback type="invalid">
               Please enter the planting season(s).
             </Form.Control.Feedback>
@@ -55,7 +108,13 @@ export default function PlantForm(props) {
 
           <Form.Group as={Col} controlId="formMaturity">
             <Form.Label>Time to Maturity</Form.Label>
-            <Form.Control required type="text" placeholder="Ex: 2 years" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Ex: 2 years"
+              defaultValue={plantInfo.maturity}
+              onChange={handleChange}
+            />
             <Form.Control.Feedback type="invalid">
               Please enter time until the plant is ready to produce fruit or
               flower.
@@ -66,7 +125,13 @@ export default function PlantForm(props) {
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formSeedSpacing">
             <Form.Label>Seed Spacing</Form.Label>
-            <Form.Control required type="text" placeholder="Ex: 10cm apart" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Ex: 10cm apart"
+              defaultValue={plantInfo.spacing}
+              onChange={handleChange}
+            />
             <Form.Control.Feedback type="invalid">
               Please enter spacing requirements for planting seeds.
             </Form.Control.Feedback>
@@ -74,7 +139,13 @@ export default function PlantForm(props) {
 
           <Form.Group as={Col} controlId="formPlantDepth">
             <Form.Label>Seed Planting Depth</Form.Label>
-            <Form.Control required type="text" placeholder="Ex: 10cm deep" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Ex: 10cm deep"
+              defaultValue={plantInfo.depth}
+              onChange={handleChange}
+            />
             <Form.Control.Feedback type="invalid">
               Please enter depth requirements for planting seeds.
             </Form.Control.Feedback>
@@ -88,6 +159,8 @@ export default function PlantForm(props) {
               required
               type="number"
               placeholder="Please enter a number"
+              defaultValue={plantInfo.maxTemp}
+              onChange={handleChange}
             />
             <Form.Control.Feedback type="invalid">
               Please enter a number.
@@ -100,6 +173,8 @@ export default function PlantForm(props) {
               required
               type="number"
               placeholder="Please enter a number"
+              defaultValue={plantInfo.minTemp}
+              onChange={handleChange}
             />
             <Form.Control.Feedback type="invalid">
               Please enter a number.
@@ -110,7 +185,12 @@ export default function PlantForm(props) {
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formSunlightNeeds">
             <Form.Label>Sunlight Needs</Form.Label>
-            <Form.Control required as="select" custom>
+            <Form.Control
+              required
+              as="select"
+              defaultValue={plantInfo.sunlight}
+              onChange={handleChange}
+            >
               <option value="">Select your plant's light requirments</option>
               <option value="Direct Sun">Direct Sun</option>
               <option value="Indirect Sun">Indirect Sun</option>
@@ -123,7 +203,12 @@ export default function PlantForm(props) {
 
           <Form.Group as={Col} controlId="formWaterNeeds">
             <Form.Label>Water needs</Form.Label>
-            <Form.Control required as="select" custom>
+            <Form.Control
+              required
+              as="select"
+              defaultValue={plantInfo.water}
+              onChange={handleChange}
+            >
               <option value="">
                 Select how many days per week you want to water:
               </option>
@@ -148,6 +233,8 @@ export default function PlantForm(props) {
             as="textarea"
             rows={3}
             placeholder="Tell us about your plant!"
+            defaultValue={plantInfo.description}
+            onChange={handleChange}
           />
           <Form.Control.Feedback type="invalid">
             Please provide a brief description.
@@ -161,3 +248,24 @@ export default function PlantForm(props) {
     </Container>
   );
 }
+
+
+//onchange info:
+//best guide:
+//https://www.agirl.codes/complete-guide-build-react-forms-with-usestate-hook
+//https://stackoverflow.com/questions/34006333/cant-type-in-react-input-text-field
+//^using defaultValue vs Value
+//https://www.brainstormcreative.co.uk/react-js/react-bootstrap-form-part-2-validation-and-errors/
+//more info on validations if we want it:
+//https://dev.to/alecgrey/controlled-forms-with-front-and-backend-validations-using-react-bootstrap-5a2
+//https://dev.to/heyjoshlee/using-the-usestate-hook-and-working-with-forms-in-react-js-m6b
+
+//as="select"
+//https://www.pluralsight.com/guides/how-to-get-select-element's-value-in-react-bootstrap
+//https://stackoverflow.com/questions/54831454/validation-for-select-elements-of-react-bootstrap-are-mis-aligned
+
+//questions:
+//it's not grabbing the values despite using state spread??
+//defaultValue vs Value
+//this - uncontrolled components?
+//why does the console log disappear even why the reset is gone?
