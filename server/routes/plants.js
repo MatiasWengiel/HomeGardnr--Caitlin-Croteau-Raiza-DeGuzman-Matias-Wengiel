7f0e6ae3ee9fc3d3d9930f6427caa80cf3516a1c
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-26 13:36:07 -0500

    Add '/plants/:query' route to search DB from user queries

--- server/routes/plants.js
+++ server/routes/plants.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { searchPlant } = require('../../db/query_functions')
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
     res.send("This would be the plants page");
   });
+
+  router.get('/:query', (req, res) => {
+    const query = req.params.query;
+    db.query(searchPlant(), [query]).then(data => console.log(data.rows))
+  })
   return router;
 };
