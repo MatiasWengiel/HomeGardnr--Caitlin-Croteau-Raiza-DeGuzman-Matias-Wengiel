67b2ddb06aa9c908aacaa26d664b116a2ddc69ef
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-26 11:03:21 -0700

    Import getUserPlantInfo function. Add route for api/user_plants/:id that retrieves a plant from a user's garden

--- server/routes/user_plants.js
+++ server/routes/user_plants.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { getUserPlantInfo } = require("../../db/query_functions.js");
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
     res.send("This would be the user_plants page");
   });
+
+  router.get("/:id", (req, res) => {
+    db.query(getUserPlantInfo(req.params.id)).then((data) => {
+      console.log(data);
+      res.json(data.rows);
+    });
+  });
+
   return router;
 };
