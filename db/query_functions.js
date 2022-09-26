b03af5c23939202f8ee19f6ee53083094047d489
Caitlin Croteau <cjcroteau@gmail.com>
2022-09-26 14:56:52 -0700

    export addPlant function

--- db/query_functions.js
+++ db/query_functions.js
@@ -4,24 +4,23 @@ const getUserLocation = (userId) => {
   `;
 };
 
-const addPlant = function(plant) {
-
-  const values =[
-    plant.generic_name,
-    plant.specifc_name,
+const addPlant = function (plant) {
+  const values = [
+    plant.genericName,
+    plant.specifcName,
     plant.description,
     plant.thumbnail_photo_url,
     plant.large_plant_card_photo_url,
-    plant.when_to_plant,
-    plant.water_needs,
-    plant.sunlight_needs,
-    plant.lowest_temp_tolerance,
-    plant.highest_temp_tolerance,
-    plant.how_far_apart_to_plant,
-    plant.how_deep_to_plant,
-    plant.how_long_until_mature
-  ]
-  
+    plant.season,
+    plant.waters,
+    plant.sunlight,
+    plant.minTemp,
+    plant.maxTemp,
+    plant.spacing,
+    plant.depth,
+    plant.maturity,
+  ];
+
   const queryString = `
   INSERT INTO plants (generic_name,
     specific_name,
@@ -36,9 +35,9 @@ const addPlant = function(plant) {
     how_far_apart_to_plant,
     how_deep_to_plant,
     how_long_until_mature)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);`
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *;`;
 
-    return [queryString, values];
-}
+  return [queryString, values];
+};
 
-module.exports = { getUserLocation };
+module.exports = { getUserLocation, addPlant };
