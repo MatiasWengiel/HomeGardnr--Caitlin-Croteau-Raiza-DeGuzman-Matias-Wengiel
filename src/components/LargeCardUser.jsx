f5accf939f1579db862369ae0a2d375e8af767bb
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-26 11:11:34 -0700

    Import Button and According from react-bootstrap. Change GET url to user_plants/1. Add plant data into card, taken from user_plant and plants tables from DB

--- src/components/LargeCardUser.jsx
+++ src/components/LargeCardUser.jsx
@@ -1,15 +1,18 @@
 import { React, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import axios from "axios";
 import "./LargeCardUser.scss";
+
+import Accordion from "react-bootstrap/Accordion";
 
 export default function LargeCardUser(props) {
   const [data, setPlantData] = useState({});
 
   useEffect(() => {
     axios
-      .get("/api/plants/1")
+      .get("/api/user_plants/1")
       // How to set the id so it's dynamic?
       .then((response) => {
         console.log(response.data[0]);
@@ -36,38 +39,65 @@ export default function LargeCardUser(props) {
           <div className="lcu-info">
             <Card.Title>{data.specific_name}</Card.Title>
             <div className="lcu-data">
-              <h6>When To Plant:</h6>
-              <p>{data.when_to_plant}</p>
+              <h6>Planted Date:</h6>
+              <p>{data.planted_date}</p>
             </div>
 
             <div className="lcu-data">
-              <h6>Water Needs:</h6>
-              <p>{data.water_needs}</p>
+              <h6>Last Watered:</h6>
+              <p>{data.last_watered_at}</p>
             </div>
 
             <div className="lcu-data">
-              <h6>Sunlight Needs:</h6>
-              <p>{data.sunlight_needs}</p>
+              <h6>When to Water Next:</h6>
+              <p>{data.when_to_water_next}</p>
             </div>
 
-            <div className="lcu-data">
-              <h6>Highest Temp Tolerance:</h6>
-              <p>{data.highest_temp_tolerance}</p>
-            </div>
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>More Plant Details</Accordion.Header>
+                <Accordion.Body>
+                  <div className="lcu-data">
+                    <h6>When To Plant:</h6>
+                    <p>{data.when_to_plant}</p>
+                  </div>
 
-            <div className="lcu-data">
-              <h6>Lowest Temp Tolerance:</h6>
-              <p>{data.lowest_temp_tolerance}</p>
-            </div>
+                  <div className="lcu-data">
+                    <h6>Water Needs:</h6>
+                    <p>{data.water_needs}</p>
+                  </div>
 
-            <div className="lcu-data">
-              <h6>Planting Distance:</h6>
-              <p>{data.how_far_apart_to_plant}</p>
-            </div>
+                  <div className="lcu-data">
+                    <h6>Sunlight Needs:</h6>
+                    <p>{data.sunlight_needs}</p>
+                  </div>
 
-            <div className="lcu-data">
-              <h6>Time to Maturity:</h6>
-              <p>{data.how_long_until_mature}</p>
+                  <div className="lcu-data">
+                    <h6>Highest Temp Tolerance:</h6>
+                    <p>{data.highest_temp_tolerance}</p>
+                  </div>
+
+                  <div className="lcu-data">
+                    <h6>Lowest Temp Tolerance:</h6>
+                    <p>{data.lowest_temp_tolerance}</p>
+                  </div>
+
+                  <div className="lcu-data">
+                    <h6>Planting Distance:</h6>
+                    <p>{data.how_far_apart_to_plant}</p>
+                  </div>
+
+                  <div className="lcu-data">
+                    <h6>Time to Maturity:</h6>
+                    <p>{data.how_long_until_mature}</p>
+                  </div>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+
+            <div className="lcu-buttons">
+              <Button variant="primary">Watered</Button>
+              <Button variant="danger">Delete</Button>
             </div>
           </div>
         </Card.Body>
