be60c2950036f285045678bf3bb54456fffee03c
Caitlin Croteau <cjcroteau@gmail.com>
2022-09-27 12:16:19 -0700

    update queries and single route that adds new plant to plant-lib and my garden

--- db/query_functions.js
+++ db/query_functions.js
@@ -9,8 +9,7 @@ const addPlantToLibrary = function (plant) {
     plant.genericName,
     plant.specificName,
     plant.description,
-    plant.thumbnail_photo_url,
-    plant.large_plant_card_photo_url,
+    plant.imageURL,
     plant.season,
     plant.water,
     plant.sunlight,
@@ -25,7 +24,6 @@ const addPlantToLibrary = function (plant) {
   INSERT INTO plants (generic_name,
     specific_name,
     description,
-    thumbnail_photo_url,
     large_plant_card_photo_url,
     when_to_plant,
     water_needs,
@@ -35,23 +33,21 @@ const addPlantToLibrary = function (plant) {
     how_far_apart_to_plant,
     how_deep_to_plant,
     how_long_until_mature)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *;`;
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *;`;
 
   return [queryString, values];
 };
 
-//add new query for update garden. take from user_plant query
-// const addPlantToMyGarden = function (plantId) {
-//   const values = [
-//
-//   ];
+//add new query for update my garden.
+const addPlantToMyGarden = function (plantId, userId) {
+  const today = "2022-09-27";
+  const tomorrow = "2022-09-28";
+  const values = [today, today, tomorrow, plantId, userId];
 
-//   const queryString = `
-//   INSERT INTO user_plants
-//  .......... query here
-//     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *;`;
+  const queryString = `
+  INSERT INTO user_plants (planted_date, last_watered_at, when_to_water_next, plant_id, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
 
-//   return [queryString, values];
-// };
+  return [queryString, values];
+};
 
-module.exports = { getUserLocation, addPlantToLibrary };
+module.exports = { getUserLocation, addPlantToLibrary, addPlantToMyGarden };
