14e6a5e3ad8254b87d721e777f3337c9627fc997
Caitlin Croteau <cjcroteau@gmail.com>
2022-09-27 11:22:35 -0700

    remove button from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Button from "./components/Button";
 import Banner from "./components/Banner";
 import PlantCard from "./components/PlantCard";
 import WeatherCard from "./components/WeatherCard";
@@ -12,14 +11,6 @@ function App() {
   return (
     <div className="App">
       <Navigation user="Waldo" />
-
-      <h1>The page is empty for now!</h1>
-
-      <Button
-        onClick={() => alert("AAAHHHHHH WHY WOULD YOU CLICK THAT BUTTON?!")}
-      >
-        Except for this button
-      </Button>
       <h2>Here is the form to add new plants!</h2>
       <PlantForm />
       <WeatherCard currentProvince="BC" />
