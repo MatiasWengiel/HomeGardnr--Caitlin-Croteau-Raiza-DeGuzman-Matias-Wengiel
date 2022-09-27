c55b8df04f186dcd6332faca89ca26107ae827bc
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-27 13:13:20 -0700

    Add p tag as label for now to show this as the visitor's view plant card. Will remove later

--- src/components/LargeCardVisitor.jsx
+++ src/components/LargeCardVisitor.jsx
@@ -24,6 +24,7 @@ export default function LargeCardVisitor(props) {
 
   return (
     <Container className="lcv-container">
+      <p>This is the VISITOR Card</p>
       <Card>
         <Card.Body className="d-flex flex-row mb-3 justify-content-around">
           <div>
