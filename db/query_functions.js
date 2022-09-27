0dbbe80ad6bfa4f5836f0a39ec9cd43ffb50b42e
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-27 13:20:15 -0700

    Add comment above getUserPlantInfo function

--- db/query_functions.js
+++ db/query_functions.js
@@ -11,6 +11,7 @@ const getPlantInfo = (plantId) => {
   `;
 };
 
+// Retrieve all info about an individual plant IN the user's plant library (user_plants)
 const getUserPlantInfo = (plantId) => {
   return `
     SELECT * FROM user_plants
