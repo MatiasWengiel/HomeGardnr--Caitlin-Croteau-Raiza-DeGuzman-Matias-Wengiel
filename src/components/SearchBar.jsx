7d8df1cc15c15b6c9ebf21f1d4449bdd53466eb1
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-29 10:34:05 -0500

    Remove unnecessary Form.Label and move margin setting to component on view

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,8 +4,7 @@ export default function SearchBar(props) {
   return (
     <>
       <Form>
-        <Form.Group className="mb-3" controlId="searchBar">
-          <Form.Label>Find your plant here</Form.Label>
+        <Form.Group controlId="searchBar">
           <Form.Control
             type="text"
             placeholder="Search for your next plant here!"
