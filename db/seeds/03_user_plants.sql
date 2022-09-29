9f7303adc7e82eb79f1ec846cbfebf60c57e6fb7
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-28 19:11:23 -0700

    Add three more plants

--- db/seeds/03_user_plants.sql
+++ db/seeds/03_user_plants.sql
@@ -5,3 +5,9 @@ INSERT INTO user_plants (planted_date, last_watered_at, when_to_water_next, plan
 INSERT INTO user_plants (planted_date, last_watered_at, when_to_water_next, plant_id, user_id) VALUES ('2022-06-01', '2022-09-19', '2022-09-23', 2, 1);
 
 INSERT INTO user_plants (planted_date, last_watered_at, when_to_water_next, plant_id, user_id) VALUES ('2022-05-19', '2022-09-19', '2022-09-23', 3, 1);
+
+INSERT INTO user_plants (planted_date, last_watered_at, when_to_water_next, plant_id, user_id) VALUES ('2022-05-19', '2022-09-19', '2022-09-23', 4, 1);
+
+INSERT INTO user_plants (planted_date, last_watered_at, when_to_water_next, plant_id, user_id) VALUES ('2022-05-19', '2022-09-19', '2022-09-23', 5, 1);
+
+INSERT INTO user_plants (planted_date, last_watered_at, when_to_water_next, plant_id, user_id) VALUES ('2022-05-19', '2022-09-19', '2022-09-23', 6, 1);
