e372fb5b8d9179cec5ae867534f4de26f46abf27
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-28 19:33:51 -0700

    Fix alignment of specific and generic names to center them once again

--- src/components/LargeCardUser.jsx
+++ src/components/LargeCardUser.jsx
@@ -78,7 +78,9 @@ export default function LargeCardUser(props) {
       <Card>
         <Card.Body className="d-flex flex-row mb-3 justify-content-around">
           <div>
-            <Card.Title>{plantData.generic_name}</Card.Title>
+            <Card.Title className="text-center">
+              {plantData.generic_name}
+            </Card.Title>
             <Card.Img
               width={400}
               height={400}
@@ -87,7 +89,9 @@ export default function LargeCardUser(props) {
             />
           </div>
           <div className="lcu-info">
-            <Card.Title>{plantData.specific_name}</Card.Title>
+            <Card.Title className="text-center">
+              {plantData.specific_name}
+            </Card.Title>
 
             <div className="lcu-data">
               <h6>Planted Date:</h6>
