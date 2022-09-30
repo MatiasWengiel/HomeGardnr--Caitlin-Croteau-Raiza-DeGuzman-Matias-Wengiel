124bda2f3ed75bedba7353a1a91c0bf531ed69d1
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-30 15:27:09 -0500

    Remove references to SearchView and replace with PlantLibrary under /plants/

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import WeatherProvider from './providers/WeatherProvider';
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import WeatherCard from "./components/WeatherCard";
 import Root from "./routes/Root";
-import PlantLibrary from "./components/PlantLibrary";
-import PlantCard from "./components/PlantCard";
 import LargeCardMain from "./components/LargeCardMain";
 import LargeCardUser from "./components/LargeCardUser";
 import UserGarden from "./components/UserGarden";
-import SearchView from "./routes/SearchView";
+import PlantLibrary from "./routes/PlantLibrary";
 
 const router = createBrowserRouter([
   {
@@ -23,7 +20,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/plants/",
-        element: <SearchView />
+        element: <PlantLibrary />
       },
       {
         path: "/plants/:id",
