fe2c7b7a97ef52a784a325008c4c5f8c8c3f9661
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-30 18:47:41 -0700

    Import AddPlantToMyGarden function. Use func in added POST route to handle when user clicks on 'Add to My Garden' btn on main library's lg plant card modal.

--- server/routes/user_plants.js
+++ server/routes/user_plants.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { getUserPlantInfo, deleteUserPlant } = require("../../db/query_functions.js");
+const { getUserPlantInfo, deleteUserPlant, addPlantToMyGarden } = require("../../db/query_functions.js");
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
     res.send("This would be the user_plants page");
+  });
+
+  // Add plant to user's garden (from plant library)
+
+  // Data passed through payload will be in 'request' object. To access it, console log req.body.
+
+  router.post("/submit", (req, res) => {
+    let newPlantId = req.body.plantId;
+    const userId = 1;
+    
+    const [queryString, values] = addPlantToMyGarden(newPlantId, userId);
+
+    db.query(queryString, values).then((data) => {
+      console.log(req.body);
+      res.send();
+    });
   });
 
   router.get("/:id", (req, res) => {
