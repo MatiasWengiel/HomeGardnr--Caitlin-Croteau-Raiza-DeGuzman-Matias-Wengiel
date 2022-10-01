9b4824439c66561862affc4dd4c6cce5f9e785d6
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-01 15:17:53 -0500

    Add main route that will return all of the user's garden

--- server/routes/my_garden.js
+++ server/routes/my_garden.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const { useContext } = require("react");
 const router = express.Router();
-const { getUserPlantInfo, deleteUserPlant, addPlantToMyGarden } = require("../../db/query_functions.js");
+const { getUserPlantInfo, deleteUserPlant, addPlantToMyGarden, getMyGardenPlants } = require("../../db/query_functions.js");
+
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
-    res.send("This would be the my_garden page");
+  router.get('/all/:userID', (req, res) => {
+    const userID = req.params.userID
+    db.query(getMyGardenPlants(userID)).then(data => res.json(data.rows))
   });
 
   // Add plant to user's garden (from plant library)
   router.post("/submit", (req, res) => {
     let newPlantId = req.body.plantId;
     const userId = 1;
-    
+
     const [queryString, values] = addPlantToMyGarden(newPlantId, userId);
 
     db.query(queryString, values).then((data) => {
@@ -29,9 +32,9 @@ module.exports = (db) => {
     db.query(deleteUserPlant(req.params.id)).then(() => {
       res.send(200);
     })
-    .catch((error) => {
-      console.log(error);
-    });
+      .catch((error) => {
+        console.log(error);
+      });
   });
 
   return router;
