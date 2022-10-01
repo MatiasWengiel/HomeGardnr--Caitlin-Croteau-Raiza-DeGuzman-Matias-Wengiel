fad14cc9802737381ed0cb68d7f424d238fd7aa9
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-30 19:29:39 -0700

    Remove generic_name column

--- db/schema/01_plants.sql
+++ db/schema/01_plants.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS plants CASCADE;
 CREATE TABLE plants (
 
   id SERIAL PRIMARY KEY NOT NULL,
-  generic_name VARCHAR(255) NOT NULL,
   specific_name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   large_plant_card_photo_url VARCHAR(255) NOT NULL,
