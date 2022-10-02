cc3af8e7853e8fd21043ecb0c0643ba145dcc60b
Raiza De Guzman <santos_dg7@hotmail.com>
2022-10-01 22:27:57 -0700

    Remove when to water next column

--- db/schema/03_user_plants.sql
+++ db/schema/03_user_plants.sql
@@ -7,7 +7,6 @@ CREATE TABLE user_plants (
   id SERIAL PRIMARY KEY NOT NULL,
   planted_date DATE NOT NULL,
   last_watered_at DATE NOT NULL,
-  when_to_water_next DATE NOT NULL,
   plant_id INTEGER REFERENCES plants(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 
