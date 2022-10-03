03efdb6ff852908e1ec4f5ad3f71e90790f55068
Raiza De Guzman <santos_dg7@hotmail.com>
2022-10-02 19:30:22 -0700

    Remove lcu-container class from Container opening bootstrap tag

--- src/components/LargeCardUser.jsx
+++ src/components/LargeCardUser.jsx
@@ -73,7 +73,7 @@ export default function LargeCardUser(props) {
   };
 
   return (
-    <Container className="lcu-container">
+    <Container>
       <p>This is the User Card</p>
       <Card>
         <Card.Body className="d-flex flex-row mb-3 justify-content-around">
