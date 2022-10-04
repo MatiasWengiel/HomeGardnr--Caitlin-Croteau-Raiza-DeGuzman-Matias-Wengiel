c427177d609bf8b5d2029686e2d753be1a22631d
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-03 20:37:35 -0500

    BUG: Previous solution for refreshing page when a card is deleted generated an infinite loop. Solution being worked on by Caitlin, will leave code as is

--- src/routes/MyGarden.jsx
+++ src/routes/MyGarden.jsx
@@ -58,10 +58,11 @@ export default function MyGarden() {
 
   useEffect(() => {
     axios.get(`/api/my_garden/all/${userID}`).then((response) => {
+      console.log(response.data);
       setGardenInfo(response.data);
       setSelectedPlants(response.data);
     });
-  }, [cardsList]);
+  }, []);
 
   return (
     <Container className="w-90">
