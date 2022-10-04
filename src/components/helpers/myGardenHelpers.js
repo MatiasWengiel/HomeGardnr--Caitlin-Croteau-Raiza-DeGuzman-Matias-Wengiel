d2718c523821be44d0ca14ee7d3fa6c11278453e
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-04 09:49:51 -0500

    Create calculateNextWaterDate and dateFormatter helper functions

---
+++ src/components/helpers/myGardenHelpers.js
@@ -0,0 +1,13 @@
+//Formats output date and removes year
+export const dateFormatter = (date) => date.toLocaleDateString(undefined, { weekday: "short", month: "short", day: "numeric" })
+
+export const calculateNextWaterDate = (lastWatered, waterNeeds) => {
+  const lastWateredDate = new Date(lastWatered);
+  //Calculates water interval by dividing 7 (days) over number of waterings needed per week (waterNeeds)
+  const waterInterval = Math.floor(7 / waterNeeds);
+  //Takes the last watered date and creates a new date that is the last watered plus the water interval
+  const waterDate = new Date(
+    lastWateredDate.setDate(lastWateredDate.getDate() + waterInterval)
+  )
+  return waterDate
+};
