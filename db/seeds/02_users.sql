b4cd33e0ed8803f57d5d6b44f4bca6f79fe08f0a
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-05 12:53:37 -0500

    Add more plants and users for db testing purposes

--- db/seeds/02_users.sql
+++ db/seeds/02_users.sql
@@ -1,3 +1,5 @@
 -- Seeds for Users Table --
 
 INSERT INTO users (id, first_name, last_name, email, password, location) VALUES (1, 'Waldo', 'Smith', 'waldos@email.com', 'abc', 'Vancouver');
+INSERT INTO users (id, first_name, last_name, email, password, location)
+VALUES (2, 'John', 'Wick', 'assassin@movies.com', 'abc', 'Winnipeg');
