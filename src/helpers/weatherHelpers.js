21459aa4afecc59f372edd8a1ed8d668294f721b
Raiza De Guzman <santos_dg7@hotmail.com>
2022-10-05 23:03:50 -0700

    Remove mock data placed as stand in when weather API calls exceeded and comments associated with it. Replace mock data variables with variables being passed in as arguments for function -- localHigh, localLow, localPrecipitation. Values for these are coming from weather API

--- src/helpers/weatherHelpers.js
+++ src/helpers/weatherHelpers.js
@@ -1,42 +1,31 @@
-
 export const checkForWeatherWarnings = (localHigh, localLow, localPrecipitation) => {
+
   // Thresholds for temperature (highest, lowest) and rain level
   const highTempThreshold = 30;
   const lowTempThreshold = 0;
   const rainLevelThreshold = 13;
 
-  /* 
-  Use function's arguments localHigh, localLow, localPrecipitation to compare against thresholds.
-  Otherwise, Use these variables when API calls exceeded for the day:
-  */
-  const dailyHighTemp = 15;
-  const dailyLowTemp = 5;
-  const dailyRain = 20;
-
-  
-  /* Approach #3: Possibly one of temperature conditions AND rain are met. So, display more than one warning message corresponding to weather conditions that were met. */
-
   // Push warning msgs into array if any of three conditions met.
   const weatherMsgs = [];
 
-  const highHeat = `TAKE ACTION -- 🥵🥵 Extreme heat expected for the day. Temperatures as high as ${dailyHighTemp} C expected.`;
+  const highHeat = `TAKE ACTION -- 🥵🥵 Extreme heat expected for the day. Temperatures as high as ${localHigh} C expected.`;
 
-  const extremeCold = `TAKE ACTION -- 🥶🥶 Freezing temperatures expected for the day. Temperatures could drop as low as ${dailyLowTemp} C.`;
+  const extremeCold = `TAKE ACTION -- 🥶🥶 Freezing temperatures expected for the day. Temperatures could drop as low as ${localLow} C.`;
 
-  const heavyRain = `HEAVY RAIN -- 🌧🌧 Heavy rainfall expected today, as much as ${dailyRain} mm.`;
+  const heavyRain = `HEAVY RAIN -- 🌧🌧 Heavy rainfall expected today, as much as ${localPrecipitation} mm.`;
 
-  if (dailyHighTemp >= highTempThreshold) {
+  if (localHigh >= highTempThreshold) {
     weatherMsgs.push(highHeat);
   }
 
-  if (dailyLowTemp <= lowTempThreshold) {
+  if (localLow <= lowTempThreshold) {
     weatherMsgs.push(extremeCold);
   }
 
-  if (dailyRain >= rainLevelThreshold) {
+  if (localPrecipitation >= rainLevelThreshold) {
     weatherMsgs.push(heavyRain);
   }
 
-  // Returns an empty array if none of conditions above met.
+  // Returns an empty array if none of conditions above are met.
   return weatherMsgs;
 };
