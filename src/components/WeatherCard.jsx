351ca0d98e5166da41278c822a1d95cdf9cbb102
Raiza De Guzman <santos_dg7@hotmail.com>
2022-10-07 14:03:24 -0700

    Remove fragment wrapped around Container. Fix indentation for Container element and all its contents

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -11,34 +11,32 @@ export default function WeatherCard(props) {
   const { currentCity, localHigh, localLow, localPrecipitation, forecastLink } = useContext(weatherContext);
 
   return (
-    <>
-      <Container className="weather-container">
-        <Alert variant="secondary">
-          Today's Weather Information for{" "}
-          <span style={{ fontWeight: "bold" }}>{currentCity}</span>
-        </Alert>
-        <Stack gap={3} style={{ display: "flex", alignItems: "start" }}>
-          <div className="weather-headings">
-            <p>Local Warnings: </p>
-            <p className="weather-values">{localWarnings || "none"}</p>
-          </div>
-          <div className="weather-headings">
-            <p>Today's High: </p>
-            <p className="weather-values">{localHigh}ºC</p>
-          </div>
-          <div className="weather-headings">
-            <p>Today's Low: </p>
-            <p className="weather-values"> {localLow}ºC</p>
-          </div>
-          <div className="weather-headings">
-            <p>Expected Precipitation: </p>
-            <p className="weather-values">{localPrecipitation || "0"} mm</p>
-          </div>
-          <div className="weather-headings">
-            <a href={forecastLink}>See full weather report at AccuWeather</a>
-          </div>
-        </Stack>
-      </Container>
-    </>
+    <Container className="weather-container">
+      <Alert variant="secondary">
+        Today's Weather Information for{" "}
+        <span style={{ fontWeight: "bold" }}>{currentCity}</span>
+      </Alert>
+      <Stack gap={3} style={{ display: "flex", alignItems: "start" }}>
+        <div className="weather-headings">
+          <p>Local Warnings: </p>
+          <p className="weather-values">{localWarnings || "none"}</p>
+        </div>
+        <div className="weather-headings">
+          <p>Today's High: </p>
+          <p className="weather-values">{localHigh}ºC</p>
+        </div>
+        <div className="weather-headings">
+          <p>Today's Low: </p>
+          <p className="weather-values"> {localLow}ºC</p>
+        </div>
+        <div className="weather-headings">
+          <p>Expected Precipitation: </p>
+          <p className="weather-values">{localPrecipitation || "0"} mm</p>
+        </div>
+        <div className="weather-headings">
+          <a href={forecastLink}>See full weather report at AccuWeather</a>
+        </div>
+      </Stack>
+    </Container>
   );
 }
