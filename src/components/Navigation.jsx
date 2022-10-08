1a0a9b6ebb6adc15e719a8d65f1ecca52530ab96
Raiza De Guzman <santos_dg7@hotmail.com>
2022-10-07 19:41:37 -0700

    Remove use of template literals for each weather event msg

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -36,13 +36,13 @@ export default function Navigation(props) {
   /* Handles user's selection from Weather Events menu. Displays Banner comp with corresponding weather event message. */
   const handleSelect = (eventKey) => {
 
-    const highHeat = `TAKE ACTION -- 🥵🥵 Extreme heat expected for the day. Temperatures as high as 35 C expected.`;
+    const highHeat = "TAKE ACTION -- 🥵🥵 Extreme heat expected for the day. Temperatures as high as 35 C expected.";
 
-    const extremeCold = `TAKE ACTION -- 🥶🥶 Freezing temperatures expected for the day. Temperatures could drop as low as -20 C.`;
+    const extremeCold = "TAKE ACTION -- 🥶🥶 Freezing temperatures expected for the day. Temperatures could drop as low as -20 C.";
 
-    const heavyRain = `HEAVY RAIN -- 🌧🌧 Heavy rainfall expected today, as much as 30 mm.`;
+    const heavyRain = "HEAVY RAIN -- 🌧🌧 Heavy rainfall expected today, as much as 30 mm.";
 
-    const sharkNado = `SHARKS -- 🦈🦈 Do not swim in the flooded, shark-infested streets`
+    const sharkNado = "SHARKS -- 🦈🦈 Do not swim in the flooded, shark-infested streets.";
 
     if (eventKey == "heat") {
       setBannerMessage([highHeat]);
