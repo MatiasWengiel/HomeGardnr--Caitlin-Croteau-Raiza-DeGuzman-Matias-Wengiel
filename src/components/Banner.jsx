4256548e28cc5b84e807ec14eb422083f7782816
Raiza De Guzman <santos_dg7@hotmail.com>
2022-10-14 13:04:21 -0700

    Remove code from prior to manually trigger banner and weather warning msgs w/ Weather Events. Add back code logic to trigger banner and warning msgs using weather API data and thresholds to compare against.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,23 +2,21 @@ import { useState } from "react";
 import { Alert } from "react-bootstrap";
 
 export default function Banner(props) {
-  const { weatherWarning, setBannerAndMsg } = props;
+  const weatherWarning = props.weatherWarning;
+  const [show, setShow] = useState(true);
 
-  return (
-    <Alert
-      className="text-center rounded-0"
-      variant="danger"
-      onClose={() => setBannerAndMsg([])}
-      dismissible
-    >
-      <span style={{ fontWeight: "bold" }}>Weather Warning:</span>
-      {weatherWarning.map((msg, index) => {
-        return (
-          <p className="m-0" key={index}>
-            {msg}
-          </p>
-        );
-      })}
-    </Alert>
-  );
+  if (show) {
+    return (
+      <Alert className="text-center rounded-0" variant="danger" onClose={() => setShow(false)} dismissible>
+        <span style={{ fontWeight: "bold" }}>Weather Warning:</span>
+        {weatherWarning.map((msg, index) => {
+          return (
+            <p className="m-0" key={index}>
+              {msg}
+            </p>
+          );
+        })}
+      </Alert>
+    );
+  }
 }
